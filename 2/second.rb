50cb60d39e8be96e9a36cacf7937224126cd717f
Nicholas Horton <nicholashorton7@protonmail.com>
2025-12-02 23:54:47 +0000

    Add procedural generation solution for second day, second part

--- 2/second.rb
+++ 2/second.rb
@@ -12,7 +12,81 @@
 # first three letters * (length of string / 3)
 # up to...
 # first (length of string / 2) letters * (length of string / (length of string / 2))
+# So brute force is easy.
 #
+# Procedural solution.
+# We can easily generate the possible valid chunk sizes for a given ID.
+# We can't have a chunk as big as the number, since it has to repeat.
+# So, starting at 2, we divide the digit count of the ID by each number until we
+# get to the digit count itself. If the remainder is 0, then the resulting factor
+# can be added to the set of possible chunk sizes.
+# E.g., for a 9-digit ID,
+# 9 % 2 != 0
+# 9 % 3 == 0. So we add the result of 9 / 3 (3) to the chunk-size set
+# 9 % 4 != 0
+# ...
+# 9 % 9 == 0, so we add 9 / 9 == 1 to the chunk-size set,
+# which is { 1, 3 }. These are the sizes of chunk that can be repeated to compose
+# the ID exactly.
+#
+# These chunk-size sets could be memoized for added efficiency, so that each time
+# our program encounters a new digit-count, we check a hash to see if we can simply
+# extract the chunk-size set rather than calculate it again.
+#
+# For any given range, we could convert the lowest number to its digit size and
+# do the same for the highest number. Then we need to procedurally generate every
+# combination of repeating chunks that can exist within the range
+#
+# E.g., 11-22
+# So the only chunk size feasible is 1, since the entire range is 2-digit.
+# Also, the description tells us that *no ID starts with zero.
+# So the first possible repeat chunk is `1`, forming `11`.
+# The second is `2` forming `22`.
+# We can't go any further without exceeding the range.
+#
+# Another example. 998-1012. The possible chunk sizes are { 1 } for 3 digits
+# and then { 1, 2 } for 4 digits.
+# How do we know where to start?
+# We don't want to generate `111` through `888` do we? We could, and simply test against
+# the lowest bound. But it might make sense to go the other way.
+# Also, it occurs to me that we should pair a chunk size with the number of
+# repeats needed to fill the digit-count when we memoize it. Anyway,
+#
+# "9" * 3 == "999". 999 is greater than the lowest bound and smaller than the highest.
+# We add that to the solution set for this range.
+# "8" * 3 == "888". 888 is lower than the lowest bound. Stop. But how do we know to stop?
+# Since when we come to the 4-digit solutions, we need to start from the bottom and
+# stop when they get too big. So we could say that we start from the top when
+# we are in the digit-count that contains the low bound. Any intermediate digit-counts
+# it doesn't matter, and then we start from the bottom when we get to the maximum
+# digit count.
+#
+# So for 4 digits, this is the size of the upper bound, so we start from the bottom.
+# The chunk sizes are 1 and 2.
+# "1" * 4 == "1111". 1111 is already higher than the bound so we stop
+# "10" * 2 == "1010". (first digit cannot be 0). 1010 is lower than high bound, so we
+# add it to the solution set.
+# "11" * 2 == "1111". 1111 is higher than the bound, so stop.
+# Therefore, the solution set for this range is { 999, 1010 }.
+#
+# E.g, 1,188,511,880 - 1,188,511,890
+# The range is 10 digit-count numbers only.
+# 10 % 2 == 0. 10 / 2 == 5.
+# 10 % 5 == 0. 10 / 5 == 2.
+# 10 % 10 == 0. 10 / 10 == 1. So the set of chunk sizes is { 1, 2, 5 }
+# So, start with `1` * 10. too low
+# `2` * 10. too high. Move on to 2-digit chunks
+# `10` * 5 == 1010101010. too low.
+# `11` * 5 == 1111111111. too low
+# `12` * 5 == 1212121212. too high. next chunk size
+# `10000` * 2 == 1000010000. too low
+# ...
+# `11885` * 2 == 1188511885. not too high or low. add to set
+# `11886` * 2 == 1188611886. too high. Stop
+#
+# Actually, the starting point should not be `10`, `100` etc.
+# The starting point should be the first n digits of the low bound. We should
+# stop when we exceed the high bound.
 # DS:
 #
 # A:
@@ -25,9 +99,11 @@
 #     * return true
 # * return false
 
+# Brute force
+
 # Parse CSV file
 class Parser
-  def self.parse(file)
+  def self.read(file)
     # rubocop:disable Style/ExpandPathArguments
     root = File.expand_path('..', __FILE__)
     # rubocop:enable Style/ExpandPathArguments
@@ -51,7 +127,8 @@ def invalid_id?(id)
   false
 end
 
-ranges = Parser.parse('input.txt')
+t = Time.now
+ranges = Parser.read('input.txt')
 acc = 0
 ranges.each do |current_range|
   current_range.each do |id|
@@ -59,6 +136,7 @@ ranges.each do |current_range|
   end
 end
 
-puts acc
+puts "Anser: #{acc}"
+puts "Time: #{Time.now - t}"
 
 # answer: 21932258645
---
+++ 2/second_second_pass.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+# Procedural generation solution
+
+# Parse CSV file
+class Parser
+  def self.read(file)
+    # rubocop:disable Style/ExpandPathArguments
+    root = File.expand_path('..', __FILE__)
+    # rubocop:enable Style/ExpandPathArguments
+    input_text = File.read(File.join(root, file))
+    range_strings = input_text.split(',')
+    range_strings.map do |range_string|
+      range_values = range_string.split('-').map(&:to_i)
+      (range_values.first..range_values.last)
+    end
+  end
+end
+
+# Generate the answer
+class InvalidIDGenerator
+  class << self
+    # Encapsulate information about a chunk and the digit-length range it applies to
+    ChunkInfo = Struct.new('ChunkInfo',
+                           :lowest, :highest, :repeats, :range)
+
+    def sum_invalid_ids(file)
+      ranges = Parser.read(file)
+      solution_set = ranges.each_with_object(Set.new) do |range, set|
+        set.merge invalid_ids(range)
+      end
+      solution_set.sum
+    end
+
+    private
+
+    def invalid_ids(range)
+      length_ranges = get_length_ranges(range)
+      length_ranges.each_with_object(Set.new) do |length_range, set|
+        set.merge invalid_ids_from_length_range(length_range)
+      end
+    end
+
+    def invalid_ids_from_length_range(length_range)
+      chunk_size_set = get_chunk_sizes(length_range)
+      chunk_infos = get_chunk_infos(chunk_size_set, length_range)
+      chunk_infos.each_with_object(Set.new) do |chunk_info, set|
+        set.merge invalid_ids_from_chunk_info(chunk_info)
+      end
+    end
+
+    def invalid_ids_from_chunk_info(chunk_info)
+      (chunk_info.lowest..chunk_info.highest).each_with_object(Set.new) do |current_chunk, set|
+        if invalid_id?(current_chunk, chunk_info)
+          invalid_id = (current_chunk.to_s * chunk_info.repeats).to_i
+          set.add invalid_id
+        end
+      end
+    end
+
+    # Get chunk info from a digit-length range and the set of possible chunk sizes
+    def get_chunk_infos(chunk_size_set, length_range)
+      digits = length_range.first.to_s.size
+      chunk_size_set.map do |chunk_size|
+        ChunkInfo.new(
+          length_range.first.to_s[0, chunk_size].to_i,
+          length_range.last.to_s[0, chunk_size].to_i,
+          digits / chunk_size,
+          length_range
+        )
+      end
+    end
+
+    def get_chunk_sizes(length_range)
+      digits = length_range.first.to_s.size
+      (2..digits).each_with_object([]) do |len, arr|
+        arr << (digits / len) if (digits % len).zero?
+      end
+    end
+
+    def get_length_ranges(range)
+      digit_counts = (range.first.to_s.length..range.last.to_s.length)
+      digit_counts.map do |count|
+        if count == digit_counts.first
+          first_length_range(count, range)
+        elsif count == digit_counts.last
+          last_length_range(count, range)
+        else
+          middle_length_range(count)
+        end
+      end
+    end
+
+    def first_length_range(count, range)
+      (range.first..(count == range.last.to_s.size ? range.last : ('9' * count).to_i))
+    end
+
+    def last_length_range(count, range)
+      "1#{'0' * (count - 1)}".to_i..range.last
+    end
+
+    def middle_length_range(count)
+      "1#{'0' * (count - 1)}".to_i..('9' * count).to_i
+    end
+
+    def invalid_id?(current_chunk, chunk_info)
+      chunk_info.range.cover? (current_chunk.to_s * chunk_info.repeats).to_i
+    end
+  end
+end
+
+t = Time.now
+puts "Answer: #{InvalidIDGenerator.sum_invalid_ids('input.txt') == 21_932_258_645}"
+puts "Time: #{Time.now - t}"
+
+# Answer: 21932258645
