eb7b395b8ccbb1df191eba859d76b2ceb70a783c
Nicholas Horton <nicholashorton7@protonmail.com>
2025-12-04 14:44:56 +0000

    Begin day 4

---
+++ 4/first.rb
@@ -0,0 +1,131 @@
+# frozen_string_literal: true
+
+# P:
+#   Count how many rolls have fewer than four neighbours in the eight
+#   adjacent positions.
+#
+# Etc:
+#
+# We know (and can confirm) that the lines are all 140 chars. This means we could
+# look for patterns if we read through the tiles as a flat sequence. I'm not sure
+# if that actually helps though (possibly just with optimization and maybe not
+# even then).
+#
+# When we consider the first index we must be careful: an algorithm that
+# decrements the indices to check adjacent squares could end up searching
+# the end of the array (index -1 etc).
+#
+# Maybe the easiest approach to start with is to write a function that,
+# given a row index and a column index, counts how many rolls are in the
+# adjacent 8 tiles.
+#
+# first, we need a way to determine which offsets are viable.
+# Or, we could take the approach of using the safe navigation operator and rely
+# on `nil`s to be NOT equal to `'@'`
+#
+# So we just need a function that takes the list of pair_offsets and iterates through
+# testing if each offset pair EXISTS and IF SO whether the offset_pair yields a roll
+#
+# DS:
+#
+# A:
+# * Parse text file into an array of arrays of chars
+# * for each row,
+# *   for each column
+# *     if count_rolls(grid, row, column) < 4
+# *       accumulate 1
+#
+
+class Parser
+  def self.read(filename)
+    # rubocop:disable Style/ExpandPathArguments
+    root = File.expand_path('..', __FILE__)
+    # rubocop:enable Style/ExpandPathArguments
+    grid = []
+    begin
+      file = File.open(File.join(root, filename))
+      file.each_line do |line|
+        grid << line.chomp.chars
+      end
+    ensure
+      file.close
+    end
+    grid
+  end
+end
+
+class RollFinder
+  ALL_OFFSETS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ].freeze
+
+  TOO_MANY_ROLLS = 4
+
+  def initialize(grid)
+    @new_grid = grid.dup
+    @grid = grid.freeze
+  end
+
+  def count_free_rolls
+    result = 0
+    @grid.each_index do |row|
+      @grid[row].each_index do |column|
+        if count_neighbouring_rolls(row, column) < TOO_MANY_ROLLS
+          result += 1
+          @new_grid[row][column] = 'x'
+        end
+      end
+    end
+    @new_grid.each do |row|
+      puts row.join
+    end
+    result
+  end
+
+  private
+
+  # The problem has to do with literal edge cases -- the tiles around the edge of the grid
+  def count_neighbouring_rolls(row, column)
+    ALL_OFFSETS.reduce(0) do |acc, (row_offset, column_offset)|
+      if valid_row?(row + row_offset) &&
+         valid_column?(row + row_offset, column + column_offset)
+        @grid[row + row_offset][column + column_offset] == '@'
+        acc + 1
+      else
+        acc
+      end
+    end
+  end
+
+  # These cannot use only nil, they must test for not nil and not negative number
+  def valid_row?(row)
+    row.positive? && @grid[row]
+  end
+
+  def valid_column?(row, column)
+    column.positive? && @grid[row][column]
+  end
+end
+
+test_input = <<~HEREDOC
+  ..@@.@@@@.
+  @@@.@.@.@@
+  @@@@@.@.@@
+  @.@@@@..@.
+  @@.@@@@.@@
+  .@@@@@@@.@
+  .@.@.@.@@@
+  @.@@@.@@@@
+  .@@@@@@@@.
+  @.@.@@@.@.
+HEREDOC
+test_input = test_input.split.map(&:chars).freeze
+
+puts RollFinder.new(test_input).count_free_rolls
